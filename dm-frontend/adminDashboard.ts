890d01e03c85b2c51e1d4f8b764d7bb45997a7a8
KhushiSarkari <60869342+KhushiSarkari@users.noreply.github.com>
2020-03-31 10:34:23 +0530

    Update adminDashboard.ts

--- dm-frontend/adminDashboard.ts
+++ dm-frontend/adminDashboard.ts
@@ -62,7 +62,7 @@ function getLatestPendingRequests(url: string) {
 
     var tableTitle = "<TH COLSPAN='4'><center>RECENT REQUESTS</center></th>";
     var tableHeading = "";
-    tableHeading += "<th class='mdl-data-table__cell--non-numeric'>Device Id</th>"
+    tableHeading += "<th class='mdl-data-table__cell--non-numeric'>Specification Id</th>"
         + "<th>Type</th>"
         + "<th>Model</th>"
         + "<th>User Id</th>";
