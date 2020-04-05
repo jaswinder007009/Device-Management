75f88ca6c260c457aea4a1dd5909f82991a37a7f
ancient-lunatic <60133632+ancient-lunatic@users.noreply.github.com>
2020-04-05 16:15:24 +0530

    remove comment

--- dm-backend/Logics/SortRequestHistoryData.cs
+++ dm-backend/Logics/SortRequestHistoryData.cs
@@ -74,17 +74,7 @@ dt.type , db.brand , dm.model_name , sp.*, rh.assign_date ,rh.assign_days , rh.r
  or concat(dt.type , ' ' , db.brand , ' ' , dm.model_name) like concat('%' ,@find ,'%')
  " + attribute + " " + sortType + " limit @low , @high ;";
             cmd.CommandType = CommandType.Text;
-            //cmd.CommandText = @"select u.user_id ,  sl.salutation , u.first_name , u.middle_name , u.last_name , u.email , rh.device_id , d.serial_number ,  
-            //dt.type , db.brand , dm.model_name , sp.*, rh.assign_date ,rh.assign_days , rh.return_date , rh.request_time  , s.status , rh.return_to as admin_user_id  , 
-            // s1.salutation as  admin_salutation  ,  u1.first_name as admin_first_Name , u1.middle_name as admin_middle_name , u1.last_name as  admin_last_name  from request_history as rh
-            // inner join status as s  inner join specification as sp  inner join user as u   inner join device_type as dt inner join device_brand as db
-            // inner join salutation as sl  inner join device_model as  dm on  dm.device_model_id= rh.device_model and db.device_brand_id = rh.device_brand and
-            // sl.salutation_id = u.salutation_id and  dt.device_type_id = rh.device_type and  sp.specification_id =  rh.specification_id and
-            // s.status_id =  rh.status_id and  u.user_id = rh.employee_id left join user u1  on  rh.return_to = u1.user_id  left join salutation as s1 on
-            // s1.salutation_id = u1.salutation_id  left join device as d  on d.device_id  = rh.device_id  where s.status like concat('%' ,@find ,'%') or d.serial_number like concat('%' ,@find ,'%') or 
-            // concat(u.first_name , ' ', if (u.middle_name is null, '' ,  concat(u.middle_name, ' ')) , u.last_name ) like concat('%' ,@find ,'%')
-            // or concat(dt.type , ' ' , db.brand , ' ' , dm.model_name) like concat('%' ,@find ,'%')
-            // limit @high , @low;";
+         
 
             BindLimitParams(cmd, low, high);
                 BindSearchParms(cmd, find);
