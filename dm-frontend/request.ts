20385483a2a62bd59b99fba250840c19d55be8c4
kaustubh-ex2 <60777148+kaustubh-ex2@users.noreply.github.com>
2020-04-07 15:36:28 +0530

    Uses base url constant

--- dm-frontend/request.ts
+++ dm-frontend/request.ts
@@ -1,6 +1,7 @@
+import { BASEURL } from "./globals";
 let adminId = 16;
 let sortDirection = 1;
-let globalUrl = "http://localhost:5000/request/";
+let globalUrl = BASEURL + "/request/";
 
 function getPendingRequests(url: string) {
 
