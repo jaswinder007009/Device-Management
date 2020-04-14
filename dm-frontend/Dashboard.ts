906605e8ba1000d78ffa1c4cd2348984d3422566
mgupta@ex2 <mgupta@ex2india.com>
2020-04-14 16:09:41 +0530

    dashboard for user and admin

--- dm-frontend/Dashboard.ts
+++ dm-frontend/Dashboard.ts
@@ -20,27 +20,31 @@ import { BASEURL, amIAdmin, amIUser } from './globals';
          <a class="mdl-navigation__link" href="deviceListForadmin.html">
              <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">devices</i>All
                  Devices
-        </a>`;
-        if (role == "Admin") {
-            let nav = ` 
+        </a>
+        
+        <a class="mdl-navigation__link" href="user-model.html">
+        <i class="mdl-color-text--blue-grey-400 material-icons"
+                role="presentation">import_export</i>Request Device
+        </a>
         <a class="mdl-navigation__link" href="userRequestHistory.html">
             <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">storage</i>My
             Devices
-        </a>
+        </a>`;
+        if (role == "Admin") {
+            let nav = ` 
+        
        
         <a class="mdl-navigation__link" href="web.html">
             <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">group</i>Users
         </a>
         <a class="mdl-navigation__link" href="adminRequestPage.html">
             <i class="mdl-color-text--blue-grey-400 material-icons"
-                role="presentation">import_export</i>Device Requests
+                role="presentation">import_export</i>All Requests
         </a>
         <a class="mdl-navigation__link" href="request-history.html">
             <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">history</i>Request History
         </a>
-        <a class="mdl-navigation__link" href="device_role/user.html">
-            <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">build</i>Roles
-        </a>
+       
         <a class="mdl-navigation__link" href="device_role/role1.html">
             <i class="mdl-color-text--blue-grey-400 material-icons"
                 role="presentation">perm_device_information</i>Permissions
@@ -49,24 +53,11 @@ import { BASEURL, amIAdmin, amIUser } from './globals';
         `;
             document.getElementById("navigation").innerHTML = navigation + nav;
         }
-        else if (role == "User") {
-            let nav = ` 
-       <a class="mdl-navigation__link" href="userRequestHistory.html">
-           <i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">storage</i>My
-           Devices
-       </a>
-       
-       
-       <a class="mdl-navigation__link" href="adminRequestPage.html">
-           <i class="mdl-color-text--blue-grey-400 material-icons"
-               role="presentation">import_export</i>Request Device
-       </a>
-    
-      
-       `;
-            document.getElementById("navigation").innerHTML = navigation + nav;
-        }
+       else
+       {
+        document.getElementById("navigation").innerHTML = navigation;
     }
+}
     function createCard(index, key, cardData) {
         var cardCreationCode: string = "<div class='demo-card-event mdl-card mdl-shadow--2dp mdl-color--blue-grey-200' id='card'>"
             + "<div class='mdl-card__title mdl-card--expand'>"
