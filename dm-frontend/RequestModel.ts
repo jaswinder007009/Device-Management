63e7a24addb6b37cc25fb975b7d37751317a66ca
Khushi Sarkari <ksarkari@ex2india.com>
2020-04-15 12:34:41 +0530

    refactoring phase-I,c-2

--- dm-frontend/RequestModel.ts
+++ dm-frontend/RequestModel.ts
@@ -1,18 +1,20 @@
-export class Requests {
-    deviceModel: string = "";
-    deviceType: string = "";
-    deviceBrand: string = "";
-    specs: Specification;
-    constructor() {
+export class Requests
+{
+    deviceModel:string ="";
+    deviceType:string = "";
+    deviceBrand:string ="";
+    specs : Specification;
+    constructor()
+    {
         this.specs = new Specification();
     }
 }
-export class Specification {
-    ram: string = "";
-    storage: string = "";
-    screenSize: string = "";
-    connectivity: string = "";
-
+export class Specification
+{
+    ram: string ="";
+    storage: string ="";
+    screenSize: string ="";
+    connectivity: string ="";
 }
 export class PartialUserModel {
     salutation: string = ""
