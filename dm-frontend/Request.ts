cea83542ea7b4c76b138829c7625916634d4af12
Khushi Sarkari <ksarkari@ex2india.com>
2020-04-15 12:23:46 +0530

    refactoring phase-I

--- dm-frontend/Request.ts
+++ dm-frontend/Request.ts
@@ -1,64 +1,35 @@
 import { BASEURL } from "./globals";
-import { Requests,Specification } from "./RequestModel";
+import { Requests, Specification, PartialUserModel } from "./RequestModel";
 let adminId = 16;
 let sortDirection = "asc";
+let globalUrl = BASEURL + "/request/pending";
 
-let globalUrl = BASEURL + "/request/";
 function getPendingRequests(url: string) {
 
     var tableData = '';
     var specs = new Specification();
+    var requestedBy = new PartialUserModel();
     fetch(url).then(Response => Response.json()).then(data => {
         for (var i = 0; i < data.length; i++) {
-            tableData += "<tr>";
-            var key, name = "";
             specs = data[i]['specs'];
-            var deviceModel = data[i]['deviceModel'];
-            var deviceType = data[i]['deviceType'];
-            var deviceBrand = data[i]['deviceBrand'];
-        
-            for (var prop in data[i]) {
-               
-                var requestId: number = data[i]['requestId'];
-                var value = data[i][prop];
-                if (typeof (value) == 'object') {
-                    for (key in value) {
-                        var val = value[key];
-						if(prop=="requestedBy"){
-                        if (key == "salutation" || key == "firstName" || key == "middleName" || key == "lastName")
-                            name += val + " ";
-						}
-                        
+            requestedBy = data[i]['requestedBy'];
+            tableData += "<tr>"
+                + "<td>" + data[i]['userId'] + "</td>"
+                + "<td>" + data[i]['deviceType'] + "</td>" + "<td>" + data[i]['deviceBrand'] + "</td>" + "<td>" + data[i]['deviceModel'] + "</td>"
+                + "<td>" + specs.ram + ", " + specs.connectivity + ",<br>" + specs.screenSize + "', " + specs.storage + "</td>"
+                + "<td>" + requestedBy.salutation + " " + requestedBy.firstName + " " + requestedBy.middleName + " " + requestedBy.lastName + "</td>"
+                + "<td>" + data[i]['requestDate'] + "</td>"
+                + "<td>" + data[i]['availability'] + "</td>";
+            if (data[i]['availability'] == true)
+                tableData += "<td>" + "<button class=\"accept-button\" data-requestid=\"" + data[i]['requestId'] + "\" >Accept</button>" + "</td>";
+            else
+                tableData += "<td>" + "<button class=\"notify-button\" data-devicemodel=\""
+                    + data[i]['deviceModel'] + "\"data-devicetype=\"" + data[i]['deviceType'] + "\" data-devicebrand=\""
+                    + data[i]['deviceBrand'] + "\"data-ram=\"" + specs.ram + "\"data-connectivity=\"" + specs.connectivity
+                    + "\"data-screensize=\"" + specs.screenSize + "\"data-storage=\"" + specs.storage + "\" >Notify</button>" + "</td>";
 
-                        if (key == "lastName")
-                            tableData += "<td>" + name + "</td>";
-						if (prop == "specs")
-                            if(key=="storage")
-								tableData += "<td>" + specs.ram+", "+ specs.connectivity +",<br>"+specs.screenSize +"", "+specs.storage+ "</td>";
-                    }
-                }
-                else {
-                    key = prop;
-                    if (key != "comment" && key != "requestId" && key != "noOfDays")
-                        tableData += "<td>" + value + "</td>";
+            tableData += "<td>" + "<button class=\"reject-button\" data-requestid=" + data[i]['requestId'] + " >Reject</button>" + "</td></tr>";
 
-                    if (key == "availability" && value == true)
-                        tableData += "<td>" + "<button class=\"accept-button\" data-requestid=\"" + requestId + "\" >Accept</button>" + "</td>";
-                     else if(key =="availability" && value == false)
-                     tableData += "<td>" + "<button class=\"notify-button\" data-devicemodel=\"" + deviceModel 
-                   +"\"data-devicetype=\"" + deviceType + "\" data-devicebrand=\"" + deviceBrand 
-                   +"\"data-ram=\"" + specs.ram
-                   +"\"data-connectivity=\"" + specs.connectivity
-                   +"\"data-screensize=\"" + specs.screenSize
-                   +"\"data-storage=\"" + specs.storage +"\" >Notify</button>" + "</td>";
-                   
-
-                    }
-            }
-
-            tableData += "<td>" + "<button class=\"reject-button\" data-requestid=" + requestId + " >Reject</button>" + "</td>";
-
-            tableData += "</tr>";
         }
         document.getElementById("content").innerHTML = tableData;
 
@@ -67,10 +38,9 @@ function getPendingRequests(url: string) {
 }
 
 function requestAction(requestUrl, requestId, action) {
-    fetch(globalUrl + requestId+requestUrl);
+    fetch(globalUrl + requestId + requestUrl);
     alert("Request " + requestId + " " + action);
-    document.getElementById("content").innerHTML = "";
-    getPendingRequests(globalUrl + "pending");
+    getPendingRequests(globalUrl);
 
 }
 
@@ -85,8 +55,7 @@ function getDirection(className, sortField) {
     }
 }
 
-function postNotification(data:Requests)
-{
+function postNotification(data: Requests) {
 
     let data1 = JSON.stringify(data);
     fetch(BASEURL + "/api/Notification", {
@@ -101,33 +70,29 @@ function postNotification(data:Requests)
     const sortField = (e.target as HTMLElement).getAttribute('name');
     const className = (document.getElementById(sortField) as HTMLTableRowElement).getAttribute("class");
     sortDirection = getDirection(className, sortField);
-    document.getElementById("content").innerHTML = "";
-    getPendingRequests(globalUrl + "pending?sortby=" + sortField + "&direction=" + sortDirection);
+    getPendingRequests(globalUrl + "?sortby=" + sortField + "&direction=" + sortDirection);
 
 });
 
 document.querySelector('#fixed-header-drawer-exp').addEventListener('input', function (e) {
     var searchField = (document.getElementById("fixed-header-drawer-exp") as HTMLInputElement).value;
-    getPendingRequests(globalUrl + "pending?search=" + searchField);
+    getPendingRequests(globalUrl + "?search=" + searchField);
 });
 
 
 document.addEventListener("click", function (e) {
 
     if ((e.target as HTMLButtonElement).className == "reject-button") {
-        const data = (e.target as HTMLButtonElement).dataset.requestid;
-        let requestId = parseInt(data, 10);
+        let requestId = parseInt((e.target as HTMLButtonElement).dataset.requestid, 10);
         requestAction('/reject?id=' + adminId, requestId, 'rejected');
 
     }
     if ((e.target as HTMLButtonElement).className == "accept-button") {
-        const data = (e.target as HTMLButtonElement).dataset.requestid;
-        let requestId = parseInt(data, 10);
+        let requestId = parseInt((e.target as HTMLButtonElement).dataset.requestid, 10);
         requestAction('/accept?id=' + adminId, requestId, 'accepted');
 
     }
-    if((e.target as HTMLButtonElement).className == "notify-button")
-    {
+    if ((e.target as HTMLButtonElement).className == "notify-button") {
         let request = new Requests();
         request.deviceModel = (e.target as HTMLButtonElement).dataset.devicemodel;
         request.deviceBrand = (e.target as HTMLButtonElement).dataset.devicebrand;
@@ -136,15 +101,14 @@ document.addEventListener("click", function (e) {
         request.specs.connectivity = ((e.target as HTMLButtonElement).dataset.connectivity);
         request.specs.screenSize = ((e.target as HTMLButtonElement).dataset.screensize);
         request.specs.storage = ((e.target as HTMLButtonElement).dataset.storage);
-         postNotification(request);
+        postNotification(request);
 
         (e.target as HTMLButtonElement).remove();
-        
+
     }
 
 });
 
-// document.getElementById("content").innerHTML = "";
-getPendingRequests(globalUrl + "pending");
+getPendingRequests(globalUrl);
 
 
--- dm-frontend/RequestModel.ts
+++ dm-frontend/RequestModel.ts
@@ -1,19 +1,29 @@
-export class Requests
-{
-    deviceModel:string ="";
-    deviceType:string = "";
-    deviceBrand:string ="";
-    specs : Specification;
-    constructor()
-    {
+export class Requests {
+    deviceModel: string = "";
+    deviceType: string = "";
+    deviceBrand: string = "";
+    specs: Specification;
+    constructor() {
         this.specs = new Specification();
     }
 }
-export class Specification
-{
-    ram: string ="";
-    storage: string ="";
-    screenSize: string ="";
-    connectivity: string ="";
-        
+export class Specification {
+    ram: string = "";
+    storage: string = "";
+    screenSize: string = "";
+    connectivity: string = "";
+
+}
+export class PartialUserModel {
+    salutation: string = ""
+    firstName: string = ""
+    middleName: string = ""
+    lastName: string = ""
+    departmentName: string = ""
+    designationName: string = ""
+    email: string = ""
+    dob: string = ""
+    gender: string = ""
+    doj: string = ""
+
 }
